4b8a54a09c15422481a2bcd4ee2932abd7ec5c5f
Alperen Özkan <alperenweb3@gmail.com>
2023-03-14 15:19:51 +0100

    styled components, splide library, map recipes

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,18 +1,56 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 function Popular() {
-    useEffect(()=>{
-        getPopular();
-    }, []);
+  const [popular, setPopular] = useState([]);
 
-    const getPopular = async () => {
-     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
-     const data = await api.json();   
-     console.log(data); 
-    }
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
+    const data = await api.json();
+    console.log(data);
+    setPopular(data.recipes);
+  };
+
   return (
-    <div>Popular</div>
-  )
+    <div>
+      <Wrapper>
+        <h3>Trending Picks</h3>
+        {popular.map((recipe) => {
+          return (
+            <Card key={recipe.id}>
+              <p>{recipe.title}</p>
+              <img src={recipe.image} alt={recipe.title} />
+            </Card>
+          );
+        })}
+      </Wrapper>
+    </div>
+  );
 }
 
-export default Popular
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  p {
+    font-size: 3rem;
+    font-weight: bold;
+  }
+
+  img {
+    border-radius: 2rem;
+  }
+`;
+
+export default Popular;
